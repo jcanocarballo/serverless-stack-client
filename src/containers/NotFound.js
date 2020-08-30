35bb4744cabbc58a0c3afeafc579eb06664b1ed4
Julian Cano <jcanoc@frd.org.mx>
2020-08-30 14:21:36 -0500

    Configuracion de nuestra App React

---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+      <h3>Lo siento, la pagina no existe!</h3>
+    </div>
+  );
+}
